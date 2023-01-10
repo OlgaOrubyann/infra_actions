7dff854f4fb3ea0673486937f6737f628ce7abbf
Ольга Отрубянникова <olgaotru@yandex.ru>
2023-01-10 10:46:58 +0300

    Test4

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -6,14 +6,25 @@ from django.test import Client, TestCase
 class StaticPagesURLTests(TestCase):
     def setUp(self):
         self.guest_client = Client()
+        self.TEMPLATES_URL_NAMES = ['', '/second/']
 
     def test_about_url_exists_at_desired_location(self):
         """Проверка доступности страниц."""
-        response = self.guest_client.get('')
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+        for address in self.TEMPLATES_URL_NAMES():
+            with self.subTest(address=address):
+                response = self.guest_client.get(address)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
 
-        response = self.guest_client.get('second/')
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+        # with self.subTest(address=''):
+        #     response = self.guest_client.get('')
+        #     self.assertEqual(response.status_code, HTTPStatus.OK)
+
+        # response = self.guest_client.get('/second/')
+        # self.assertEqual(response.status_code, HTTPStatus.OK)
+
+
+# response = self.guest_client.get(address)
+#                 self.assertEqual(response.status_code, HTTPStatus.OK)
 
     # def test_page_shows_correct_content(self):
     #     """Проверка контента страниц."""
